c8c41d9cc1464f00ee5838b6e477e9225d3bd417
Luiz França <luizdaniel.r.f@gmail.com>
2017-08-06 01:24:25 -0300

    Added screen to add new classes, displaying users classes on home screen

---
+++ src/app/add-discipline/add-discipline.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import * as firebase from 'firebase/app';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-discipline',
+  templateUrl: './add-discipline.component.html',
+  styleUrls: ['./add-discipline.component.scss']
+})
+export class AddDisciplineComponent implements OnInit {
+
+	icon: string;
+	name: string;
+	id: any;
+	uid: any;
+
+	loading: boolean = false;
+
+  error: string;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  addDiscipline() {
+  	this.loading = true;
+
+  	this.icon = "notebook";
+
+  	this.uid = this.authService.user.uid;
+
+  	const ref = firebase.database().ref('subjects').push();
+
+  	this.id = ref.key;
+
+  	ref.set({
+    	icon: this.icon,
+    	id: this.id,
+    	name: this.name,
+    	user: this.uid
+    }).then(value => {
+      this.router.navigate(['/']);
+    });;
+  }
+}
+
