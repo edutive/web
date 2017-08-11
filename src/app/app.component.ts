69f3cd3db29c1ffbb7e464e353392268d650c70d
Paulo Menezes <paulo.hgmenezes@gmail.com>
2017-08-11 00:11:17 -0300

    Menu, submenu and courses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
 
 import { AuthService } from './auth/auth.service';
+import { Discipline } from './discipline/discipline';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,29 @@ import { AuthService } from './auth/auth.service';
 })
 export class AppComponent implements OnInit {
   openMenu: boolean = false;
+  disciplineID: string;
+  discipline: Discipline;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private af: AngularFireDatabase, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.openMenu = false;
+
+    this.router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        const parts = val.url.split('/');
+        const disciplines = parts && parts.length >= 3 && parts[1] === 'discipline' && parts[2] !== 'add';
+
+        if (disciplines) {
+          this.disciplineID = parts[2];
+          this.af.object('/subjects/' + parts[2]).subscribe(subject => {
+            this.discipline = subject;
+          });
+        } else {
+          this.discipline = undefined;
+        }
+      }
+    });
 
     if (localStorage.getItem('user')) {
       this.authService.user = JSON.parse(localStorage.getItem('user'));
