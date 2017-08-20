eb94bf48339cd0fc063dc52862d7cc7975af17ee
Luiz França <luizdaniel.r.f@gmail.com>
2017-08-20 17:56:26 -0300

    Implemented chat feature

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import * as firebase from 'firebase/app';
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-messages',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+	messageText: any;
+	lastMessageDate: any;
 
-  ngOnInit() {
+  messages: FirebaseListObservable<any>;
+  usersList: any;
+  users: {};
+  uid: any;
+  userID: any; // other user
+  chatID: any;
+
+  constructor(private af: AngularFireDatabase, private authService: AuthService, private router: Router) {}
+
+  send() {
+  	this.lastMessageDate = new Date().getTime();
+
+    firebase.database().ref('messages/' + this.chatID + '/' + this.lastMessageDate).set({
+      date: this.lastMessageDate,
+      sender: this.uid,
+      text: this.messageText
+    });
+
+    this.af
+      .object('chats/' + this.uid + "/" + this.chatID)
+      .update({
+      	chat: this.chatID,
+      	lastMessage: this.messageText,
+      	message: this.lastMessageDate,
+      	user: this.userID
+      });
+
+    this.af
+      .object('chats/' + this.userID + "/" + this.chatID)
+      .update({
+      	chat: this.chatID,
+      	lastMessage: this.messageText,
+      	message: this.lastMessageDate,
+      	user: this.uid
+      });
+    
+    this.messageText = '';
   }
 
+  ngOnInit() {
+    this.chatID = this.router.url.split('/')[2];
+    this.uid = this.authService.user.uid;
+
+    firebase.database().ref('users').once('value', snapshot => {
+      this.users = snapshot.val();
+    });
+
+    firebase.database().ref('chats/' + this.uid + "/" + this.chatID).once('value', snapshot => {
+      this.userID = snapshot.val().user;
+    });
+
+    this.messages = this.af.list('messages/' + this.chatID);
+  }
 }
